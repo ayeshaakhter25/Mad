56e116c512017cede6bf6cf69107dcb1762ccc45
ayeshaakhter25 <ayeshasherani2@gmail.com>
2025-02-20 15:07:37 +0500

    first task

---
+++ first.js
@@ -0,0 +1,18 @@
+let objArr=[
+    {
+        name:'mariam',
+        age: 19
+    },
+    {
+        name: 'abbas',
+        age: 20
+    },
+    {
+        name: 'hamid',
+        age: 20
+    }
+];
+function print_data(data){
+    console.log(data);
+}
+objArr.map(print_data);
